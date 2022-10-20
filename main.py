9585341ea5d355d5d47aca543c0344fe4bd02f88
Jonatas Santos Da Silva <66139215+Jonattaz@users.noreply.github.com>
2022-10-20 00:53:49 +0000

    Criação de variáveis e como exibi-las na tela

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # Primeiro projeto python!!!
-# 
-'''
- Comentario em bloco
-'''
-print("Alo mundo!")
+'''Comentario em bloco'''
+
+print("Alo mundo!\n")
+
+# Não precisa declarar o tipo de variável
+# String
+nome = "Jonatas"
+sobrenome = "Santos"
+# Integer
+idade = 21
+
+# str() - converte para string
+print(nome + " " + sobrenome + " " + str(idade) + " de idade")
